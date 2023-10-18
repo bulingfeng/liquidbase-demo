19ec58b7c9d9704e2e6c871ef75e4ea08088b37a
Lingfeng Bu <413957294@qq.com>
2023-10-18 22:31:49 +0800

    Initial commit

---
+++ src/main/java/org/example/liquidbase/Main.java
@@ -0,0 +1,15 @@
+package org.example.liquidbase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author bulingfeng
+ * @Date: ${YEAR}/${MONTH}/${DAY}
+ */
+@SpringBootApplication
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+}
